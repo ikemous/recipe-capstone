16f4d1e708549b34b9019cd62bc16256b5b29f94
Ike Barranco <barranco.ike@outlook.com>
2021-01-19 08:54:17 -0700

    Added Recipe Reducer Skeleton

--- client/src/utils/reducers/recipeReducer.ts
+++ client/src/utils/reducers/recipeReducer.ts
@@ -1,1 +1,16 @@
-import { Recipe }from"../types";
+import { Recipe, RecipeAction } from"../types";
+
+const initialState = {
+  name: "" 
+};
+
+const recipeReducer = (state:Recipe = initialState, {type, action}:RecipeAction):Recipe => {
+  switch(type) {
+    case "":
+      return {...state}
+    default:
+      return state;
+  };
+};
+
+export default recipeReducer;
