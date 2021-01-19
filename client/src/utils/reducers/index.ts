480cbade31a356eeb1b9dc8e4dcac63c7b5b6070
Ike Barranco <barranco.ike@outlook.com>
2021-01-19 08:55:27 -0700

    Added Recipe Reducer

--- client/src/utils/reducers/index.ts
+++ client/src/utils/reducers/index.ts
@@ -1,7 +1,8 @@
 import { combineReducers } from "redux";
+import recipeReducer from "./recipeReducer";
 
 const allReducers = combineReducers({
-
+  recipe: recipeReducer,
 });
 
 export default allReducers;
