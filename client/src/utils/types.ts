82901dafb54445c002c058e11748c762e66f4a45
Ike Barranco <barranco.ike@outlook.com>
2021-01-19 09:13:38 -0700

    Added types for Query

--- client/src/utils/types.ts
+++ client/src/utils/types.ts
@@ -6,3 +6,12 @@ export interface RecipeAction {
     type: string;
     action: string;
 };
+
+export interface Query {
+  query: string;  
+};
+
+export interface QueryAction {
+    type: string;
+    action: string;
+};
