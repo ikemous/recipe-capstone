96d821e7c003ec30c8ffca7ee1ea337760e0a22e
Ike Barranco <barranco.ike@outlook.com>
2021-01-20 12:57:46 -0700

    Added skeleton of Navbar

--- client/src/components/TotallyCewlNavBar.tsx
+++ client/src/components/TotallyCewlNavBar.tsx
@@ -1,7 +1,18 @@
 import React from "react";
+import { Navbar, NavbarToggler, Collapse, Nav, NavItem, NavLink, NavbarBrand } from "reactstrap";
 
 function TotallyCewlNavBar() {
-  return <></>
+  return (
+    <nav>
+      <Navbar expand="sm">
+        <NavbarBrand>uCook!</NavbarBrand>
+        <NavbarToggler />
+        <Collapse>
+          
+        </Collapse>
+      </Navbar>
+    </nav>
+  );
 };
 
 export default TotallyCewlNavBar;
