105dee049af54a2d33f989d2ffd3b30394b26e08
Ike <barranco.ike@gmail.com>
2021-01-22 16:38:56 -0700

    added skeleton for form

---
+++ client/src/components/AightUserAuthenticationForm.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import { Form } from "reactstrap";
+
+function AightUserAuthenticationForm() {
+    return (
+        <Form>
+        </Form>
+    );
+};
+
+export default AightUserAuthenticationForm;
