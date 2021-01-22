14b890d7aa42924369d8388d48debc1a66dc5b99
Ike <barranco.ike@gmail.com>
2021-01-22 15:26:04 -0700

    added landing page

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,12 +1,11 @@
 import React from 'react';
 import './App.css';
 import QueryForm from "./components/QueryForm";
+import LandingPage from "./pages/LandingPage";
 
 function App() {
   return (
-    <div className="App">
-      <QueryForm />
-    </div>
+    <LandingPage></LandingPage>
   );
 }
 
