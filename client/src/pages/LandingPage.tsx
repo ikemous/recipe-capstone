5b7fe9b5d9766e95a0fe6c85b5c0f6d30a150a4b
Ike <barranco.ike@gmail.com>
2021-01-22 16:35:09 -0700

    added skeleton for landing page

--- client/src/pages/LandingPage.tsx
+++ client/src/pages/LandingPage.tsx
@@ -8,12 +8,24 @@ function LandingPage() {
        <QueryForm />
        <div>
          <h2>Search For Recipes!</h2>
+         <p>
+           You can either search from the home page or click the search page! Think of an ingredient or 
+           food choice and discover some delicious meals!
+         </p>
        </div>
        <div>
          <h2>Save Favourites!</h2>
+         <p>
+           Found something you like but not hungry yet? Click the heart and save it for later!
+           You'll need to be logged in. Not Logged in? Create and account!
+         </p>
        </div>
        <div>
          <h2>Save Your Own Recipes!</h2>
+         <p>
+           Know of some recipes but dont want to write them on paper? Create your recipe and save it for later!
+           You'll need to be logged in. Not Logged in? Create and account!
+         </p>
        </div>
     </PageContainer>
   );
