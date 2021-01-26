e20b0f9e3cdaf2d4799edb27f719c462bf646544
Ike <barranco.ike@gmail.com>
2021-01-25 17:10:19 -0700

    added typescript to FC

--- client/src/components/AightUserAuthenticationForm.tsx
+++ client/src/components/AightUserAuthenticationForm.tsx
@@ -1,20 +1,38 @@
 import React from "react";
-import { Form, FormGroup, Button } from "reactstrap";
+import { Form, FormGroup, Button, Card, CardTitle } from "reactstrap";
 
-function AightUserAuthenticationForm() {
+interface Props {
+    signupPage?: boolean;
+}
+
+const AightUserAuthenticationForm:React.FC<Props> = (props) => {
+
+    const { signupPage } = props;
     return (
-        <Form>
-            <FormGroup>
-                
-            </FormGroup>
-            <FormGroup>
+        <Card>
+            <CardTitle>
+                {
+                    signupPage?
+                    "Signup":
+                    "Login"
+                }
+            </CardTitle>
+            <Form>
+                <FormGroup>
+                    
+                </FormGroup>
+                <FormGroup>
 
-            </FormGroup>
-            <FormGroup>
-                <Button></Button>
-            </FormGroup>
-        </Form>
+                </FormGroup>
+                <FormGroup>
+                    <Button></Button>
+                </FormGroup>
+            </Form>
+        </Card>
     );
 };
 
+AightUserAuthenticationForm.defaultProps = {
+    signupPage: false,
+};
 export default AightUserAuthenticationForm;
