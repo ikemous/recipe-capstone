084fac45620583b8462e2a94490a35667893c898
Ike <barranco.ike@gmail.com>
2021-01-27 14:40:31 -0700

    fixed env

--- client/src/firebase.js
+++ client/src/firebase.js
@@ -5,10 +5,10 @@ const app = firebase.initializeApp({
     apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
     authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
     projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
-    storageBucket: process.env.REACT_APP_FIREBASE_,
-    messagingSenderId: process.env.REACT_APP_FIREBASE_,
-    appId: process.env.REACT_APP_FIREBASE_,
-    measurementId: process.env.REACT_APP_FIREBASE_
+    storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.REACT_APP_FIREBASE_APP_ID,
+    measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID
 });
 
 export const auth = app.auth();
