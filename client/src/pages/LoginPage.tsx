1d3929b1607c5229a17637498e12a01a27349357
Ike <barranco.ike@gmail.com>
2021-01-27 14:42:09 -0700

    started Login Page

---
+++ client/src/pages/LoginPage.tsx
@@ -0,0 +1,13 @@
+import  React from "react";
+import AightUserAuthenticationForm from "../components/AightUserAuthenticationForm";
+import PageContainer from "../components/PageContainer";
+
+function LoginPage() {
+    return (
+        <PageContainer>
+            <AightUserAuthenticationForm />
+        </PageContainer>
+    );
+};
+
+export default LoginPage;
