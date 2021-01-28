b8b08f04e8fcef8ab6f455b9310b3511d1e4466b
Ike <barranco.ike@gmail.com>
2021-01-27 20:09:33 -0700

    added logout component

--- client/src/components/LogoutButton.tsx
+++ client/src/components/LogoutButton.tsx
@@ -1,16 +1,24 @@
 import React from "react";
 import { Button } from "reactstrap";
 import { useAuth } from "../utils/contexts/AuthContext";
+import { useHistory } from "react-router-dom";
 
 function LogoutButton() {
-    
-    function handleLogout() {
+    const { logout } = useAuth();
+    const history = useHistory();
 
+    async function handleLogout() {
+        try {
+            await logout();
+            history.push("/");
+        } catch {
+            console.log("Failed To Logout :(");
+        }
     }
 
     return (
         <Button onClick={handleLogout}>
-
+            Logout
         </Button>
     );
 };
