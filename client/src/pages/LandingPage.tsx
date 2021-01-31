8a82b4a410e40b8187137f60c3f9112a4a8bd37b
ike barranco <barranco.ike@outlook.com>
2021-01-30 22:04:40 -0700

    redisigned code same look

--- client/src/pages/LandingPage.tsx
+++ client/src/pages/LandingPage.tsx
@@ -1,5 +1,4 @@
 import React from "react";
-import PageContainer from "../components/PageContainer";
 import QueryForm from "../components/QueryForm";
 import FluidImage from "../components/FluidImage";
 import { Col, Container, Row, } from "reactstrap";
@@ -9,14 +8,52 @@ import TotallyCewlNavBar from "../components/TotallyCewlNavBar";
 function LandingPage() {
   return (
     <>
-    <TotallyCewlNavBar />
-    <Container style={{padding: 0, margin: 0}} fluid={true}>
-      <QueryForm />
-    </Container>
-    <Container fluid={true}>
-
-    </Container>
-
+      <TotallyCewlNavBar />
+      <Container style={{padding: 0, margin: 0}} fluid={true}>
+        <QueryForm />
+      </Container>
+      <Container fluid={true}>
+        <Row style={{ marginLeft: 0, marginRight: 0 }} className="item-section">
+          <Col className="text-center" sm={12} md={4}>
+            <FluidImage />
+          </Col>
+          <Col sm={12} md={8}>
+            <Row>
+              <h2 className="col text-center">Search For Recipes!</h2>
+            </Row>
+            <Row>
+              <p className="col text-center">
+                You can either search from the home page or click the search page! Think of an ingredient or 
+                food choice and discover some delicious meals!
+              </p>
+            </Row>
+          </Col>
+        </Row>
+        <Row className="item-section">
+          <Col xs={{ size: "auto", order: 2}}  sm={{ size: "8", order: 1}}>
+            <h2 className="text-center">Save Favourites!</h2>
+            <p className="col text-center">
+              Found something you like but not hungry yet? Click the heart and save it for later!
+              You'll need to be logged in. Not Logged in? Create and account!
+            </p>
+          </Col>
+          <Col className="text-center" xs={{ size: "auto", order: 1}} sm={{ size: "4", order: 2}}>
+            <FluidImage />
+          </Col>
+        </Row>
+        <Row className="item-section">
+          <Col className="text-center" sm={12} md={4}>
+            <FluidImage />
+          </Col>
+          <Col  sm={12} md={8}>
+            <h2 className="text-center">Save Your Own Recipes!</h2>
+            <p className="col text-center">
+              Know of some recipes but dont want to write them on paper? Create your recipe and save it for later!
+              You'll need to be logged in. Not Logged in? Create and account!
+            </p>
+          </Col>
+        </Row>
+      </Container>
     </>
   );
 };
