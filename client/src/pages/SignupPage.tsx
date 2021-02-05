9875744313ecb2e8028ba5ae54497a8780fcabe8
ikemous <barranco.ike@outlook.com>
2021-02-05 14:43:32 -0700

    centered page

--- client/src/pages/SignupPage.tsx
+++ client/src/pages/SignupPage.tsx
@@ -1,11 +1,21 @@
 import React from "react";
 import AightUserAuthenticationForm from "../components/AightUserAuthenticationForm";
 import PageContainer from "../components/PageContainer";
+import { Row, Col } from "reactstrap";
 
 function SignupPage() {
     return (
         <PageContainer>
-            <AightUserAuthenticationForm signupPage />
+            <Row 
+                style={{
+                    alignItems: "center", 
+                    height: "75vh"
+                }}
+            >
+                <Col>
+                    <AightUserAuthenticationForm signupPage />
+                </Col>
+            </Row>
         </PageContainer>
     );
 };
