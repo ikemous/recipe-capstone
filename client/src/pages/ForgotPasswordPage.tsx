22666a1da729611b38d136cd27b5f79636249781
ikemous <barranco.ike@outlook.com>
2021-02-05 14:44:39 -0700

    centered page

--- client/src/pages/ForgotPasswordPage.tsx
+++ client/src/pages/ForgotPasswordPage.tsx
@@ -1,11 +1,21 @@
 import React from "react";
 import PageContainer from "../components/PageContainer";
 import ForgotPasswordForm from "../components/ForgotPasswordForm";
+import { Row, Col } from "reactstrap";
 
 function ForgotPasswordPage() {
     return (
         <PageContainer>
-            <ForgotPasswordForm />
+            <Row 
+                style={{
+                    alignItems: "center", 
+                    height: "75vh"
+                }}
+            >
+                <Col>  
+                    <ForgotPasswordForm />
+                </Col>
+            </Row>
         </PageContainer>
     );
 };
