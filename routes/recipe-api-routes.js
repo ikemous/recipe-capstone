2d1eab2a1a5bdbb0a2f795deca4c303b9d15dec4
Ikemous <barranco.ike@outlook.com>
2021-02-16 15:12:57 -0800

    added route for recipe traverse

--- routes/recipe-api-routes.js
+++ routes/recipe-api-routes.js
@@ -4,7 +4,6 @@ const queryExample = require("../queryExmaple.json");
 
 router.get("/recipes/:ingredient", (req, res) => {
     const { ingredient } = req.params
-    console.log(req.params);
     axios.get(`https://api.edamam.com/search?q=${ingredient}&app_id=${process.env.EDMAM_API_ID}&app_key=${process.env.EDMAM_API_KEY}`)
     .then(results => {
         console.log(results);
@@ -13,6 +12,18 @@ router.get("/recipes/:ingredient", (req, res) => {
     .catch(error => {
         res.json(error);
     });
+});
+
+router.get("/recipes/:ingredient/:page", (req, res) => {
+    console.log(req.params);
+    const { ingredient, page } = req.params;
+    const start = parseInt(page) * 10;
+    const end = start + 10;
+    axios.get(`https://api.edamam.com/search?q=${ingredient}&app_id=${process.env.EDMAM_API_ID}&app_key=${process.env.EDMAM_API_KEY}&from=${start}&to=${end}`)
+    .then(({data}) => {
+        res.json(data);
+    })
+    .catch(error => res.json(error));
 });
 
 router.get("/recipes", (req, res) => res.json(queryExample));
