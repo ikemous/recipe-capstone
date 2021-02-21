84db7f1ef0052b372bb1f5891ce9cd972d7b4995
Ikemous <barranco.ike@outlook.com>
2021-02-20 22:09:25 -0700

    added about page to routes

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -7,6 +7,7 @@ import LoginPage from './pages/LoginPage';
 import PrivateRoute from "./components/PrivateRoute";
 import NonUserRoute from "./components/NonUserRoute";
 import ForgotPasswordPage from "./pages/ForgotPasswordPage";
+import AboutPage from "./pages/AboutPage";
 import SearchPage from "./pages/SearchPage";
 import SavedRecipesPage from "./pages/SavedRecipesPage";
 import MyRecipesPage from './pages/MyRecipesPage';
@@ -20,6 +21,7 @@ function App() {
         <Switch>
           <Route exact path="/" component={LandingPage} />
           <Route exact path="/search" component={SearchPage} />
+          <Route exact path="/about" component={AboutPage} />
           <NonUserRoute path="/signup" component={SignupPage} />
           <NonUserRoute path="/login" component={LoginPage} />
           <NonUserRoute path="/forgot-password" component={ForgotPasswordPage} />
