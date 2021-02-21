fff6b2f5ad04d00aa042627015758bdf6c1b0847
Ikemous <barranco.ike@outlook.com>
2021-02-20 20:09:51 -0700

    changed node_env to all caps

--- server.js
+++ server.js
@@ -1,16 +1,15 @@
 const express = require("express");
-require("dotenv").config(); //.env use
 const path = require("path");
+require("dotenv").config(); //.env use
+
 const PORT = process.env.PORT || 3001;
 const server = express();
+
 const db = require("./models");
 
-console.log(process.env.Node_ENV);
+console.log(process.env.NODE_ENV);
 
-if(process.env.Node_ENV === "production") {
-    server.use(express.static("client/build"));
-    server.use(express.static("client/build/static"));
-}
+if(process.env.NODE_ENV === "production") server.use(express.static("client/build"));
 
 server.use(express.urlencoded({ extended: true }));
 server.use(express.json());
