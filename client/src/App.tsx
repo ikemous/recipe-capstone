2bf34e35f1a818893faff901fb039593f71f09da
Ikemous <barranco.ike@outlook.com>
2021-02-20 17:45:02 -0700

    removed unused item

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import './App.css';
 import LandingPage from "./pages/LandingPage";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import { AuthProvider } from './utils/contexts/AuthContext';
