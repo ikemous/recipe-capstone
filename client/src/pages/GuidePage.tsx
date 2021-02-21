a9035128a52d851c71d817a6f9d922f2c65676dc
Ikemous <barranco.ike@outlook.com>
2021-02-21 16:30:17 -0700

    added guide page skeleton

---
+++ client/src/pages/GuidePage.tsx
@@ -0,0 +1,12 @@
+import Recat from "react";
+import PageContainer from "../components/PageContainer";
+
+function GuidePage() {
+    return (
+        <PageContainer>
+            
+        </PageContainer>
+    )
+};
+
+export default GuidePage;
