d5b4be2f73ccef44210be7cef741366f2171cf0a
Ikemous <barranco.ike@outlook.com>
2021-02-20 22:06:14 -0700

    added skeleton for about page

---
+++ client/src/pages/AboutPage.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import PageContainer from "../components/PageContainer";
+
+function AboutPage() {
+    return (
+        <PageContainer>
+            
+        </PageContainer>
+    );
+};
+
+export default AboutPage;
