6daf535a613e0267b1db97d6bc7fe9e9d3763b13
Ikemous <barranco.ike@outlook.com>
2021-02-20 23:09:42 -0700

    added image

--- client/src/pages/AboutPage.tsx
+++ client/src/pages/AboutPage.tsx
@@ -1,10 +1,19 @@
 import React from "react";
+import { Row } from "reactstrap";
 import PageContainer from "../components/PageContainer";
 
 function AboutPage() {
     return (
         <PageContainer>
-            
+            <h2 className="text-center">About The Creator</h2>
+            <Row 
+                style={{marginRight: "auto", justifyContent: "center"}}
+            >
+                <img 
+                    src="https://via.placeholder.com/400" 
+                    alt="creator next to motorcycle"
+                />
+            </Row>
         </PageContainer>
     );
 };
